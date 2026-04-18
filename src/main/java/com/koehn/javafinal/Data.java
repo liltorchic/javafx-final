64a0aa09d173baee77f6924ce4495c53bb8027cc
Alex Koehn <mysteryman208@gmail.com>
2026-04-18 13:59:22 -0400

    modified api key format

--- src/main/java/com/koehn/javafinal/Data.java
+++ src/main/java/com/koehn/javafinal/Data.java
@@ -2,15 +2,16 @@ package com.koehn.javafinal;
 
 import java.time.format.DateTimeFormatter;
 
+import static com.koehn.javafinal.FLRApplication.apikey;
+
 public class Data
 {
-    private static final String api_key = KEY.API_KEY;
 
     public static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static String build_url(String start, String end)
     {
-        return "https://api.nasa.gov/DONKI/FLR?startDate=" + start + "&endDate=" + end + "&api_key=" + api_key;
+        return "https://api.nasa.gov/DONKI/FLR?startDate=" + start + "&endDate=" + end + "&api_key=" + apikey;
     }
 
 }
