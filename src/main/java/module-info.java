3735c86f4463b689bb10bcf0d5a4436137b2b530
Alex Koehn <mysteryman208@gmail.com>
2026-04-18 11:28:43 -0400

    updated Jackson dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,14 @@ module com.koehn.javafxfinal {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
-    requires jackson.core;
-    requires jackson.databind;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
     requires java.desktop;
     requires java.sql;
 
     opens com.koehn.javafinal to javafx.fxml;
     exports com.koehn.javafinal;
+    exports com.koehn.javafinal.controller;
+    opens com.koehn.javafinal.controller to javafx.fxml;
 }
